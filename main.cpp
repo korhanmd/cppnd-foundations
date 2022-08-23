bfffdce21dc887b42e2cff032f0b6beb2c93e519
Korhan Mutludoğan <korhanmd@gmail.com>
2022-08-23 12:00:05 +0300

    Implement a function to print the board

--- main.cpp
+++ main.cpp
@@ -4,10 +4,21 @@
 using std::cout;
 using std::vector;
 
+void PrintBoard(const vector<vector<int>> board) {
+    for (vector<int> line : board) {
+        for (int value : line) {
+            cout << value;
+        }
+        cout << "\n";
+    }
+}
+
 int main() {
     vector<vector <int>> board{{0, 1, 0, 0, 0, 0},
                                 {0, 1, 0, 0, 0, 0},
                                 {0, 1, 0, 0, 0, 0},
                                 {0, 1, 0, 0, 0, 0},
                                 {0, 0, 0, 0, 1, 0}};
+
+    PrintBoard(board);
 }
